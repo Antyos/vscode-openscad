893b227840bbbf25c3809928ebe267c04a9dc922
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2021-12-28 13:02:52 -0600

    style: Rename extension-node separator `-` to `.`

--- webpack.config.ts
+++ webpack.config.ts
@@ -11,7 +11,7 @@ const nodeConfig: Configuration = {
     mode: 'none',
     // Entry point into extension (in package.json). See: https://webpack.js.org/configuration/entry-context/
     entry: {
-        'extension-node': './src/node/extension-node.ts',
+        'extension.node': './src/node/extension.node.ts',
     },
     // Bundle output location. See: https://webpack.js.org/configuration/output/
     output: {
@@ -57,7 +57,7 @@ const browserConfig: Configuration = {
     ...nodeConfig,
     target: 'webworker',
     entry: {
-        'extension-web': './src/web/extension-web.ts',
+        'extension.web': './src/web/extension.web.ts',
         // 'test/suite/index': './src/web/test/suite/index.ts',
     },
     output: {
