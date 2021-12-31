518f11f277cb5ab52d16deda04e77f5c5952db39
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2021-12-30 23:43:51 -0600

    fix: Type issues with getStyleUri() Simplified CheatsheetStyles class

--- src/common/cheatsheet/cheatsheet-content.ts
+++ src/common/cheatsheet/cheatsheet-content.ts
@@ -1,25 +1,25 @@
 import { HTMLElement, parse } from 'node-html-parser';
 import * as vscode from 'vscode';
-
-/** Cheatsheet color schemes. Located in `[extensionPath]/media/cheatsheet/` */
-const colorScheme = {
-    original: 'cheatsheet-original.css',
-    auto: 'cheatsheet-auto.css',
-};
-
-const DEFAULT_STYLE = 'auto';
+import { CheatsheetStyles } from './styles';
 
 /** Get HTML content of the OpenSCAD cheatsheet */
 export class CheatsheetContent {
     /** Uri to the directory containing the cheatsheet html and style sheets */
     private readonly _cheatsheetUri: vscode.Uri;
+    /** HTMLElement of document */
+    private _document?: HTMLElement;
     /** Stored copy of the cheatsheet HTML document */
     private _content?: string = undefined;
     /** The last key used to get the cheatsheet stylesheet */
     private _lastStyleKey?: string = undefined;
+    /** Styles container */
+    private _cheatsheetStyles: CheatsheetStyles;
 
     public constructor(cheatsheetUri: vscode.Uri) {
         this._cheatsheetUri = cheatsheetUri;
+
+        // ! This is WRONG. We need extensionUri, but I'm lazy right now.
+        this._cheatsheetStyles = new CheatsheetStyles(cheatsheetUri);
     }
 
     /** Get cheatsheet HTML content. Stores HTML from lastStyleKey. */
@@ -51,37 +51,22 @@ export class CheatsheetContent {
         return this._lastStyleKey;
     }
 
-    /** Get the file URI to the style sheet */
-    private getStyleSheetUri(styleKey: string): vscode.Uri {
-        // Get the filename of the given colorScheme
-        // Thank you: https://blog.smartlogic.io/accessing-object-attributes-based-on-a-variable-in-typescript/
-        const styleSource =
-            styleKey in colorScheme
-                ? colorScheme[styleKey as keyof typeof colorScheme]
-                : colorScheme[DEFAULT_STYLE];
-
-        // Get style sheet URI
-        return vscode.Uri.joinPath(this._cheatsheetUri, styleSource).with({
-            scheme: 'vscode-resource',
-        });
-        // console.log("Style" + styleUri); // DEBUG
-    }
-
     /**
      * Get a <link> HTMLElement for a stylesheet.
-     * @param stylesheetRef Key to lookup the desired stylesheet
+     * @param {string} href Reference to stylesheet
+     * @param {string | undefined} id of element
      * @returns HTMLElement
      */
-    private getStyleSheetElement(stylesheetReference: string): HTMLElement {
+    private getStyleSheetElement(href: string, id?: string): HTMLElement {
         // HTMLElement `parent` argument cannot be type 'undefined', so we have
         // to disable the check here
         // eslint-disable-next-line unicorn/no-null
-        const element = new HTMLElement('link', { id: '' }, '', null);
+        const element = new HTMLElement('link', { id: id || '' }, '', null);
         const attributes = {
             type: 'text/css',
             rel: 'stylesheet',
-            href: stylesheetReference,
-            media: 'all',
+            href: href,
+            media: 'none', // We will turn on specific stylesheets later
         };
 
         element.setAttributes(attributes);
@@ -107,15 +92,17 @@ export class CheatsheetContent {
             element.remove();
         }
 
-        // Get uri of stylesheet
-        const styleReference = this.getStyleSheetUri(styleKey).toString();
+        for (const styleKey of this._cheatsheetStyles) {
+            // Create new style element
+            const newStyle = this.getStyleSheetElement(
+                this._cheatsheetStyles.styles[styleKey].toString(),
+                styleKey
+            );
 
-        // Create new style element
-        const newStyle = this.getStyleSheetElement(styleReference);
-
-        // Append style element
-        // eslint-disable-next-line unicorn/prefer-dom-node-append
-        head.appendChild(newStyle);
+            // Append style element
+            // eslint-disable-next-line unicorn/prefer-dom-node-append
+            head.appendChild(newStyle);
+        }
 
         // Return document as html string
         return htmlDocument.toString();
--- src/common/cheatsheet/styles.ts
+++ src/common/cheatsheet/styles.ts
@@ -17,32 +17,25 @@ export const STYLES = {
 
 type StyleKey = keyof typeof STYLES;
 
-function isStyleKey(key: string): key is StyleKey {
-    return key in STYLES;
-}
-
 /** Default style */
 export const DEFAULT_STYLE: StyleKey = 'auto';
 
-export class Styles {
-    private readonly _extensionUri: vscode.Uri;
-    private _styles: { [key in StyleKey]: vscode.Uri };
-    public defaultStyle: vscode.Uri;
+export class CheatsheetStyles {
+    public readonly styles: { [key in StyleKey]: vscode.Uri };
+    public readonly defaultStyle: vscode.Uri;
 
     // Allow `___ of Styles` to iterate over _styles
-    *[Symbol.iterator]() {
-        yield* Object.keys(this._styles);
+    *[Symbol.iterator](): Iterator<StyleKey> {
+        yield* Object.keys(this.styles) as StyleKey[];
     }
 
     public constructor(extensionUri: vscode.Uri) {
-        this._extensionUri = extensionUri;
-
         // Map STYLES to Uris relative to `extensionUri`.
         //
         // Note: because we are compiling to ES6, we can't use
         // Object.fromEntries(), so we have to emulate it:
         // https://stackoverflow.com/a/43682482
-        this._styles = Object.assign(
+        this.styles = Object.assign(
             {},
             ...Object.entries(STYLES).map(([styleKey, stylePath]) => {
                 return {
@@ -52,15 +45,6 @@ export class Styles {
         );
 
         // Set the default style
-        this.defaultStyle = this._styles[DEFAULT_STYLE];
-    }
-
-    /** Get Uri to stylesheet based on a key. Returns undefined if invalid key. */
-    public getStyleUri<T extends StyleKey | string>(
-        styleKey: T
-    ): T extends StyleKey ? vscode.Uri : undefined {
-        return isStyleKey(styleKey)
-            ? this._styles[styleKey as keyof typeof STYLES]
-            : undefined;
+        this.defaultStyle = this.styles[DEFAULT_STYLE];
     }
 }
