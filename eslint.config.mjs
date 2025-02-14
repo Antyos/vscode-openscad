23241d65a55364ee940113e00ec3534d5caad3a0
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2025-02-14 13:34:31 +0200

    chore(eslint): Update flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,64 +1,54 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-plugin-prettier";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import _import from "eslint-plugin-import";
-import unicorn from "eslint-plugin-unicorn";
-import { fixupPluginRules } from "@eslint/compat";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslint from '@eslint/js';
+import eslintPluginImport from 'eslint-plugin-import';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
-    ignores: ["**/*.d.ts", "dist/*"],
-}, ...compat.extends(
-    "vscode-ext",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:unicorn/recommended",
-    "prettier",
-), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-        prettier,
-        "simple-import-sort": simpleImportSort,
-        import: fixupPluginRules(_import),
-        unicorn,
-    },
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 6,
-        sourceType: "module",
-    },
-
-    rules: {
-        "prettier/prettier": ["warn", {
-            endOfLine: "auto",
-            singleQuote: true,
-        }],
-
-        "simple-import-sort/imports": ["warn", {
-            groups: [["^\\u0000"], ["^@?\\w"], ["^"], ["^src", "^\\."]],
-        }],
-
-        "simple-import-sort/exports": "warn",
-        "import/first": "error",
-        "import/newline-after-import": "error",
-        "import/no-duplicates": "error",
-        "no-trailing-spaces": "warn",
-        "eol-last": ["warn", "always"],
-        "unicorn/prefer-node-protocol": "off",
-        "unicorn/import-style": "off",
-    },
-}];
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    prettierRecommended,
+    eslintPluginImport.flatConfigs.recommended,
+    {
+        ignores: ['**/*.d.ts', 'dist/*'],
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            'simple-import-sort': simpleImportSort,
+            unicorn: eslintPluginUnicorn,
+        },
+        languageOptions: {
+            parser: tseslint.parser,
+            ecmaVersion: 6,
+            sourceType: 'module',
+        },
+        rules: {
+            'prettier/prettier': [
+                'warn',
+                {
+                    endOfLine: 'auto',
+                    singleQuote: true,
+                },
+            ],
+            'simple-import-sort/imports': [
+                'warn',
+                {
+                    groups: [['^\\u0000'], ['^@?\\w'], ['^'], ['^src', '^\\.']],
+                },
+            ],
+            'simple-import-sort/exports': 'warn',
+            'import/first': 'error',
+            'import/newline-after-import': 'error',
+            'import/no-duplicates': 'error',
+            'no-trailing-spaces': 'warn',
+            'eol-last': ['warn', 'always'],
+            // 'unicorn/import-style': 'off',
+            // From vscode-ext preset at:
+            // https://github.com/alefragnani/eslint-config-vscode-ext/blob/master/index.js
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-non-null-assertion': 'warn',
+        },
+    }
+);
