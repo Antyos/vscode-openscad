1010190b726208c47001a1afda1e59da23f36870
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2025-02-14 14:08:09 +0200

    chore(eslint): Revert(ish) back to compat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,49 @@
-import eslint from '@eslint/js';
-import eslintPluginImport from 'eslint-plugin-import';
-import prettierRecommended from 'eslint-plugin-prettier/recommended';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import prettier from 'eslint-plugin-prettier';
+import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import pluginImport from 'eslint-plugin-import';
+import unicorn from 'eslint-plugin-unicorn';
+import { fixupPluginRules } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-    prettierRecommended,
-    eslintPluginImport.flatConfigs.recommended,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: eslint.configs.recommended,
+    allConfig: eslint.configs.all,
+});
+
+export default [
     {
         ignores: ['**/*.d.ts', 'dist/*'],
+    },
+    ...compat.extends(
+        'vscode-ext',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:unicorn/recommended',
+        'prettier'
+    ),
+    {
         plugins: {
             '@typescript-eslint': tseslint.plugin,
-            'simple-import-sort': simpleImportSort,
-            unicorn: eslintPluginUnicorn,
+            prettier,
+            'simple-import-sort': pluginSimpleImportSort,
+            import: fixupPluginRules(pluginImport),
+            unicorn,
         },
+
         languageOptions: {
             parser: tseslint.parser,
             ecmaVersion: 6,
             sourceType: 'module',
         },
+
         rules: {
             'prettier/prettier': [
                 'warn',
@@ -30,25 +52,23 @@ export default tseslint.config(
                     singleQuote: true,
                 },
             ],
+
             'simple-import-sort/imports': [
                 'warn',
                 {
                     groups: [['^\\u0000'], ['^@?\\w'], ['^'], ['^src', '^\\.']],
                 },
             ],
+
             'simple-import-sort/exports': 'warn',
             'import/first': 'error',
             'import/newline-after-import': 'error',
             'import/no-duplicates': 'error',
             'no-trailing-spaces': 'warn',
             'eol-last': ['warn', 'always'],
-            // 'unicorn/import-style': 'off',
-            // From vscode-ext preset at:
-            // https://github.com/alefragnani/eslint-config-vscode-ext/blob/master/index.js
-            '@typescript-eslint/explicit-module-boundary-types': 'off',
-            '@typescript-eslint/no-unused-vars': 'warn',
-            '@typescript-eslint/no-explicit-any': 'warn',
-            '@typescript-eslint/no-non-null-assertion': 'warn',
+            // We don't need these disabled since we're now on Nodejs>16
+            'unicorn/prefer-node-protocol': 'off',
+            'unicorn/import-style': 'off',
         },
-    }
-);
+    },
+];
